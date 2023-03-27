95fb615e0961d35d92c18e296569418bca4ea92c
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-03-27 17:34:44 +0200

    :sparkles: add new page + refactor to Vue2 syntaxe

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,9 +5,7 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
+export default {
   name: 'IndexPage'
-})
+}
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,10 @@ const routes = [
         path: "",
         component: () => import("pages/IndexPage.vue"),
       },
+      {
+        path: "bets/public",
+        component: () => import("pages/PublicBets.vue"),
+      },
     ],
   },
 
