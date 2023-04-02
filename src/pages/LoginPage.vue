59fc20079c651e3616c27a070b23aa56b5e78a7e
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-04-02 21:13:45 +0200

    :poop: try to make a simple login form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,16 +1,62 @@
 <template>
   <q-page class="flex flex-center">
-    Login
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <q-form class="flex flex-center column" ref="loginForm">
+      <q-input
+        name="username"
+        rounded
+        outlined
+        label="Nom d'utilisateur"
+        autofocus
+        class="q-mb-md"
+        type="text"
+        v-model="username"
+        :rules="[(val) => val.length <= 0 || 'Veullez remplir tous les champs']"
+        lazy-rules
+      ></q-input>
+      <q-input
+        name="password"
+        rounded
+        outlined
+        label="Mot de passe"
+        class="q-mb-md"
+        type="password"
+        v-model="password"
+        :rules="[(val) => val.length <= 0 || 'Veullez remplir tous les champs']"
+        lazy-rules
+      ></q-input>
+      <q-btn
+        label="Se connecter"
+        type="submit"
+        color="secondary"
+        rounded
+        @click.prevent="onsubmit()"
+      />
+    </q-form>
   </q-page>
 </template>
 
 <script>
+// import { cp } from "fs";
+
 export default {
   name: "LoginPage",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onsubmit() {
+      console.log("onsubmit");
+      this.$refs.loginForm.validate().then((success) => {
+        if (success) {
+          console.log("success");
+        } else {
+          console.log("error");
+        }
+      });
+    },
+  },
 };
 </script>
