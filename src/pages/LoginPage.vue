c30e67b131135e321cc1db52128ce91f7dbb40d9
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-04-03 12:30:50 +0200

    :lipstick: fix login form style

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -2,14 +2,16 @@
   <q-page class="flex flex-center">
     <q-img src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px"></q-img>
     <q-form class="flex flex-center column" ref="loginForm">
-      <q-input name="username" rounded outlined label="Nom d'utilisateur" autofocus class="q-mb-sm" type="text"
+      <q-input name="username" rounded outlined label="Nom d'utilisateur" autofocus class="q-mb-md" type="text"
         v-model="username" :rules="[(val) => val.length > 0 || 'Veullez remplir ce champ']" lazy-rules :input-style="{
-          with: 300
-        }"></q-input>
-      <q-input name="password" rounded outlined label="Mot de passe" class="q-mb-sm" type="password" v-model="password"
-        :rules="[(val) => val.length > 0 || 'Veullez remplir ce champ']" lazy-rules></q-input>
+          width: '250px'
+        }" hide-bottom-space></q-input>
+      <q-input name="password" rounded outlined label="Mot de passe" class="q-mb-md" type="password" v-model="password"
+        :rules="[(val) => val.length > 0 || 'Veullez remplir ce champ']" lazy-rules :input-style="{
+          width: '250px'
+        }" hide-bottom-space></q-input>
       <q-btn label="Se connecter" type="submit" color="secondary" rounded @click.prevent="onsubmit()" :loading="loading"
-        padding="sm xl" />
+        padding="sm 50px" size="18px" />
     </q-form>
   </q-page>
 </template>
