40e53a615456368e42d4ee6300902508bf0a4668
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-04-03 09:10:10 +0200

    :adhesive_bandage: finish login form validation feat

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,36 +1,15 @@
 <template>
   <q-page class="flex flex-center">
+    <q-img src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px"></q-img>
     <q-form class="flex flex-center column" ref="loginForm">
-      <q-input
-        name="username"
-        rounded
-        outlined
-        label="Nom d'utilisateur"
-        autofocus
-        class="q-mb-md"
-        type="text"
-        v-model="username"
-        :rules="[(val) => val.length <= 0 || 'Veullez remplir tous les champs']"
-        lazy-rules
-      ></q-input>
-      <q-input
-        name="password"
-        rounded
-        outlined
-        label="Mot de passe"
-        class="q-mb-md"
-        type="password"
-        v-model="password"
-        :rules="[(val) => val.length <= 0 || 'Veullez remplir tous les champs']"
-        lazy-rules
-      ></q-input>
-      <q-btn
-        label="Se connecter"
-        type="submit"
-        color="secondary"
-        rounded
-        @click.prevent="onsubmit()"
-      />
+      <q-input name="username" rounded outlined label="Nom d'utilisateur" autofocus class="q-mb-sm" type="text"
+        v-model="username" :rules="[(val) => val.length > 0 || 'Veullez remplir ce champ']" lazy-rules :input-style="{
+          with: 300
+        }"></q-input>
+      <q-input name="password" rounded outlined label="Mot de passe" class="q-mb-sm" type="password" v-model="password"
+        :rules="[(val) => val.length > 0 || 'Veullez remplir ce champ']" lazy-rules></q-input>
+      <q-btn label="Se connecter" type="submit" color="secondary" rounded @click.prevent="onsubmit()" :loading="loading"
+        padding="sm xl" />
     </q-form>
   </q-page>
 </template>
@@ -44,16 +23,19 @@ export default {
     return {
       username: "",
       password: "",
+      loading: false
     };
   },
   methods: {
     onsubmit() {
-      console.log("onsubmit");
+      this.loading = true
       this.$refs.loginForm.validate().then((success) => {
         if (success) {
-          console.log("success");
+          console.log(`username: ${this.username}
+          password: ${this.password}`);
         } else {
           console.log("error");
+          this.loading = false
         }
       });
     },
