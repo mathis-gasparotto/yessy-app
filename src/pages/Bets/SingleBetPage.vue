7955b89ba8f96ccd164f4b18def3ce26a2919080
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-04-24 18:06:36 +0200

    :construction: single bet

--- src/pages/Bets/SingleBetPage.vue
+++ src/pages/Bets/SingleBetPage.vue
@@ -1,15 +1,41 @@
 <template>
-  <q-page class="flex flex-center column page bg-2">
-    <div class="home-cat-title">
-      <p class="text-h6">Chauffe toi sur ces paris</p>
-      <span class="underliner"></span>
-    </div>
-    <BetList class="bet-list-component" />
+  <q-page class="flex flex-center column page single-bet">
+    <q-list class="single-bet__props">
+      <q-item class="single-bet__prop">
+        <span class="single-bet__prop-icon-container">
+          <img class="single-bet__prop-icon" :src="bet.category.iconPath" >
+        </span>
+        <p class="single-bet__prop-text">{{ bet.category.title }}</p>
+      </q-item>
+      <q-item class="single-bet__prop">
+        <span class="single-bet__prop-icon-container">
+          <img class="single-bet__prop-icon" src="~assets/quasar-logo-vertical.svg" >
+        </span>
+        <p class="single-bet__prop-text">{{ bet.description }}</p>
+      </q-item>
+      <q-item class="single-bet__prop" v-if="bet.customReward">
+        <span class="single-bet__prop-icon-container">
+          <img class="single-bet__prop-icon" src="~assets/quasar-logo-vertical.svg" >
+        </span>
+        <p class="single-bet__prop-text">{{ bet.customReward }}</p>
+      </q-item>
+      <q-item class="single-bet__prop" v-else-if="bet.tokenRewardOdd">
+        <span class="single-bet__prop-icon-container">
+          <img class="single-bet__prop-icon" src="~assets/quasar-logo-vertical.svg" >
+        </span>
+        <p class="single-bet__prop-text">La quote est de {{ bet.tokenRewardOdd }}</p>
+      </q-item>
+      <q-item class="single-bet__prop">
+        <span class="single-bet__prop-icon-container">
+          <img class="single-bet__prop-icon" src="~assets/quasar-logo-vertical.svg" >
+        </span>
+        <p class="single-bet__prop-text">{{ bet.endAt }}</p>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-import BetList from 'src/components/BetList.vue'
 import { useRoute } from 'vue-router'
 
 export default {
@@ -23,28 +49,31 @@ export default {
   name: 'SingleBetPage',
   components: {
     BetList
+  },
+  data() {
+    return {
+      bet: {
+        id: 1,
+        title: 'Bet 1',
+        description: 'Description 1',
+        author: {
+          id: 1,
+          pseudo: 'John Doe',
+          avatarPath: '~assets/quasar-logo-vertical.svg'
+        },
+        category: {
+          id: 1,
+          title: 'Sport',
+          iconPath: '~assets/quasar-logo-vertical.svg'
+        },
+        customReward: 'Le gagnant obtiendra un skin',
+        endAt: '2023-03-11T12:00:00'
+      },
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.home {
-  &-cat-title {
-    position: relative;
-    p {
-      position: relative;
-      z-index: 1;
-      font-weight: bold;
-    }
-    .underliner {
-      position: absolute;
-      top: 20px;
-      left: -10px;
-      width: 110%;
-      background-color: $secondary;
-      height: 12px;
-      display: inline-block;
-    }
-  }
-}
+
 </style>
