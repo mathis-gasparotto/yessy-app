248cbe3e1c1f6be8c44787dd54d0017bbddc2d85
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-05-13 20:23:20 +0200

    :construction: Account page

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="page-container">
-    <q-page class="flex flex-center">
-      <div class="page-content">
+    <q-page class="flex flex-center page">
+      <div class="page-content" v-if="user.avatar">
+        <div class="account__avatar-container">
+          <q-img :src="user.avatar.imgUrl" class="account__avatar-img" />
+        </div>
         {{ user.uid }}
         {{ user.email }}
         {{ user.username }}
-        {{ format.dateFormatFromBDD(user.birthday) }}
+        {{ user.birthday }}
         {{ user.referralCode }}
         {{ user.newsletter }}
       </div>
@@ -14,8 +17,9 @@
 </template>
 
 <script>
-import { LocalStorage } from 'quasar'
+import { Loading, LocalStorage, Notify } from 'quasar'
 import createFormat from '../stores/formatting.js'
+import { getAvatar, getUser } from 'src/boot/firebase'
 
 export default {
   name: 'AccountPage',
@@ -25,9 +29,54 @@ export default {
       format: createFormat()
     }
   },
-  created() {
-    this.user = LocalStorage.getItem('user')
-    console.log(this.user)
+  async created() {
+    Loading.show()
+    await getUser(LocalStorage.getItem('user').uid).then((user) => {
+      LocalStorage.set('user', user)
+      this.user = user
+    }).catch(() => {
+      Notify.create({
+        message: 'Une erreur est survenue',
+        color: 'negative',
+        icon: 'report_problem',
+        timeout: 5000
+      })
+      Loading.hide()
+    })
+    getAvatar(this.user.avatarId).then((avatar) => {
+      this.user.avatar = avatar
+      console.log(this.user)
+      Loading.hide()
+    }).catch(() => {
+      Notify.create({
+        message: 'Une erreur est survenue',
+        color: 'negative',
+        icon: 'report_problem',
+        timeout: 5000
+      })
+      Loading.hide()
+    })
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.account {
+  &__avatar {
+    &-container {
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+      margin: auto;
+    }
+    &-img {
+      border-radius: 50%;
+      margin: auto;
+    }
+  }
+}
+.page {
+  align-items: start;
+  padding-top: 20px;
+}
+</style>
