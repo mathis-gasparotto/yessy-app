558fc1a11de517071e9d66d689d355aecd71fea7
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-05-29 17:55:00 +0200

    :sparkles: Share bet + copy access code

--- quasar.config.js
+++ quasar.config.js
@@ -28,7 +28,8 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
-      'firebase'
+      'firebase',
+      'capacitor'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
