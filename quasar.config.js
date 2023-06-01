52471e56c610284016bf1fa1c966d4bbe3af1c0d
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-06-01 19:56:24 +0200

    :construction: Build in progress

--- quasar.config.js
+++ quasar.config.js
@@ -170,7 +170,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true
+      // hideSplashscreen: true
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
